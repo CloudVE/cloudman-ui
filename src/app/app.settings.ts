a0a956c35c5d01ebe5468628baaf687076ef43d5
Nuwan Goonasekera <nuwan.ag@gmail.com>
2018-06-14 03:08:16 +0530

    Fixed proxy endpoints for backend

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,5 +1,5 @@
 export class AppSettings {
-    public static get CLOUDLAUNCH_API_ENDPOINT(): string { return '/cloudlaunch/api/v1'; }
+    public static get CLOUDLAUNCH_API_ENDPOINT(): string { return '/cloudman/api/v1'; }
     public static get CLOUDMAN_SERVER_ROOT(): string { return '/cloudman'; }
     public static get CLOUDMAN_API_ENDPOINT(): string { return `${AppSettings.CLOUDMAN_SERVER_ROOT}/api/v1`; }
     public static get CLOUDMAN_SUPPORT_LINK(): string { return 'mailto:help@cloudve.org'; }
