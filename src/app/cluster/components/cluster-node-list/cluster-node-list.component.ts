cd1afb6362ef9bb5e98c37644243e4473be410b3
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2019-04-21 00:35:23 +0530

    Show animation when fetching cluster nodes

--- src/app/cluster/components/cluster-node-list/cluster-node-list.component.ts
+++ src/app/cluster/components/cluster-node-list/cluster-node-list.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
-import { map, startWith, switchMap } from "rxjs/operators";
+import { Observable, Subject, timer, merge } from 'rxjs';
+import { map, tap, switchMap } from "rxjs/operators";
 
 import { ClusterService } from "../../../shared/services/cluster.service";
 import { ClusterNode}  from "../../../shared/models/cluster";
@@ -15,13 +15,15 @@ export class ClusterNodeListComponent {
 
     nodeObservable: Observable<ClusterNode[]>;
     nodeChanged = new Subject();
+    public fetchingNodes = false;
 
     constructor(private clusterService: ClusterService) {
-        this.nodeObservable = this.nodeChanged.pipe(
-            startWith(null),
+        this.nodeObservable = merge(timer(0, 10000), this.nodeChanged).pipe(
+            tap(() => { this.fetchingNodes = true; }),
             switchMap( () => clusterService.getClusters()),
             map(clusters => clusters[0]),
-            switchMap(cluster => this.clusterService.getClusterNodes(cluster.id))
+            switchMap(cluster => this.clusterService.getClusterNodes(cluster.id)),
+            tap(() => { this.fetchingNodes = false; }),
         )
     }
 
