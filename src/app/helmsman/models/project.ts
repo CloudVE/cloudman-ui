a437476e13c323df66fe10e9978742945823c70e
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2019-11-03 16:28:21 +0530

    Only allow project admins to add or change charts

--- src/app/helmsman/models/project.ts
+++ src/app/helmsman/models/project.ts
@@ -1,10 +1,24 @@
 import { Chart } from './chart';
 
+export interface ProjectPermissions {
+    add_project: boolean;
+    change_project: boolean;
+    delete_project: boolean;
+}
+
 export class Project {
     id: string;
     name: string;
+    permissions: ProjectPermissions;
+}
+
+export interface ProjectChartPermissions {
+    add_chart: boolean;
+    change_chart: boolean;
+    delete_chart: boolean;
 }
 
 export class ProjectChart extends Chart {
     project: Project;
+    permissions: ProjectChartPermissions;
 }
