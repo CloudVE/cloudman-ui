a039d399e4a61fc36c7fab2c6a702fb4ddd18e7c
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2021-12-17 11:55:32 +0530

    Simplify base path handling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,5 @@ export class AppRoutingModule { }
 
 
 export function getBaseLocation() {
-    let paths: string[] = location.pathname.split('/').splice(1, 1);
-    let basePath: string = (paths && paths[0]) || '';
-    return '/' + basePath;
+    return './';
 }
